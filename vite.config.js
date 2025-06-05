3004b22353b4202607834bc748117bd645dea481
Random <rc786986@gmail.com>
2025-06-05 14:35:46 +0530

    config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Allows access from LAN
+    port: 5173,      // Optional: default is 5173
+  },
+  preview: {
+    allowedHosts: ['medical-student-app-frontend.onrender.com']
+  }
 })
+
+
